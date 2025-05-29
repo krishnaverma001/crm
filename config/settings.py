9498252b159dc393ae2a4f74790439756249d74f
krishnaverma001 <krishnaverma2004.kv@gmail.com>
2025-05-30 01:06:55 +0530

    Fixed Whitenoise

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
